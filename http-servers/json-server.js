bfb934386dfacaba661db92ca6ee1f156b20c3be
Igor Godlevskyi <igor_godlevskyi@epam.com>
2019-03-04 15:41:23 +0200

    Fixed pr issues

--- http-servers/json-server.js
+++ http-servers/json-server.js
@@ -1,22 +1,30 @@
 import * as http from 'http';
 
+const port = 3000;
 const server = http.createServer();
 const product = {
-    id: 1,
-    name: 'Supreme T-Shirt',
-    brand: 'Supreme',
-    price: 99.99,
-    options: [
-        {color: 'blue'},
-        {size: 'XL'},
-    ],
+  id: 1,
+  name: 'Supreme T-Shirt',
+  brand: 'Supreme',
+  price: 99.99,
+  options: [
+    { color: 'blue' },
+    { size: 'XL' },
+  ],
 };
 
 server.on('request', (req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'application/json',
-    });
-    res.end(JSON.stringify(product));
+  res.writeHead(200, {
+    'Content-Type': 'application/json',
+  });
+  res.end(JSON.stringify(product));
+});
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('Error: ', err);
+  }
+  console.log(`Server is listening on ${port}`);
 });
 
 export default server;
