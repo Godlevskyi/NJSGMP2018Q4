807fbed71d2102d51503ef14ce3165e9a4b736ea
Igor Godlevskyi <igor_godlevskyi@epam.com>
2019-04-02 19:45:20 +0300

    Added swagger

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ import parsedCookies from './middlewares/parsedCookies';
 import parsedQuery from './middlewares/parsedQuery';
 import bodyParser from 'body-parser';
 import apiRouter from './router/apiRouter';
-
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger/swagger.json';
 
 const app = express();
 
@@ -11,7 +12,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(parsedCookies);
 app.use(parsedQuery);
-
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api', apiRouter);
 
 
